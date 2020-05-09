845aededa469cc02079cdefb345d7377b61e990e
Sergio Tellez <sergiotellezsanchez@gmail.com>
2020-05-08 22:56:02 -0500

    adding 3rd party hoster

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -21,7 +21,7 @@ class Register extends React.Component {
 	}
 
 		onSumbitSignIn = () => {
-		fetch('http://localhost:3000/register',{
+		fetch('https://ancient-retreat-13147.herokuapp.com/register',{
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class signin extends React.Component {
 	}
 
 	onSumbitSignIn = () => {
-		fetch('http://localhost:3000/signin',{
+		fetch('https://ancient-retreat-13147.herokuapp.com/signin',{
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
